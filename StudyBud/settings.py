0201dd932c4e155124e995026d0db54777c8e293
Digyat Baral <91931315+Digyat@users.noreply.github.com>
2024-11-14 13:01:06 +0545

    Update settings.py

--- StudyBud/settings.py
+++ StudyBud/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-//baig gcin otbt syuv
+# Notepad+ -> baig gcin otbt syuv
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
